c0408bad6a41db3e746fab07f9b3182f2cad90be
flatironschool <danliu277@gmail.com>
2020-07-07 15:55:14 -0400

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PurchaseOrder.destroy_all
+Sales.destroy_all
+Vendor.destroy_all
+Game.destroy_all
+Employee.destroy_all
+Customer.destroy_all
+
+10.times do
+    Customer.create(name: Faker::Name.name, address: Faker::Address.full_address, email: Faker::Internet.email)
+end
+
+10.times do
+    Employee.create(name: Faker::Name.name, address: Faker::Address.full_address, email: Faker::Internet.email, username: Faker::Internet.username, password_digest: '1')
+end
+
+10.times do
+    Game.create(name: Faker::Game.name, genre: Faker::Game.genre, sales_price: rand(2) == 1 ? 40.00 : 60.00, vendor_cost: rand(2) == 1 ? 30.00 : 45.00)
+end
+
+10.times do
+    Vendor.create(name: Faker::Company.name, address: Faker::Address.full_address, email: Faker::Internet.email)
+end
+
+5.times do
+    game_offset = rand(Game.count)
+    game_record = Game.offset(game_offset).first
+
+    vendor_offset = rand(Vendor.count)
+    vendor_record = Vendor.offset(vendor_offset).first
+
+    employee_offset = rand(Employee.count)
+    employee_record = Employee.offset(employee_offset).first
+
+    PurchaseOrder.create(game: game_record, vendor: vendor_record, employee: employee_record, quantity: Faker::Number.number(digits: 3))
+end
+
+5.times do
+    game_offset = rand(Game.count)
+    game_record = Game.offset(game_offset).first
+
+    customer_offset = rand(Customer.count)
+    customer_record = Customer.offset(customer_offset).first
+
+    employee_offset = rand(Employee.count)
+    employee_record = Employee.offset(employee_offset).first
+
+    Sales.create(game: game_record, customer: customer_record, employee: employee_record, quantity: Faker::Number.number(digits: 1))
+end
