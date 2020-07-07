037dc2e8bb4ed4840ffd54a0ee0fe0c8a54c70cc
flatironschool <danliu277@gmail.com>
2020-07-07 15:50:10 -0400

    Fix table name

--- db/migrate/003_create_game.rb
+++ db/migrate/003_create_game.rb
@@ -2,7 +2,7 @@ class CreateGame < ActiveRecord::Migration[6.0]
   def change
     create_table :games do |t|
       t.string :name
-      t.string :genere
+      t.string :genre
       t.decimal :sales_price
       t.decimal :vendor_cost
       t.integer :stock
