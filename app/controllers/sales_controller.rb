6c3ece79157be5e4a360d415013b80fe4eb95617
flatironschool <danliu277@gmail.com>
2020-07-16 17:00:19 -0400

    Reverse order

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -16,7 +16,7 @@ class SalesController < ApplicationController
             today = Date.today
             {
                 'game' => Game.find(k),
-                'sales' => [0,1,2,3,4,5,6,7,8,9,10,11].map do |x|
+                'sales' => [11,10,9,8,7,6,5,4,3,2,1,0].map do |x|
                     new_date = today.prev_month(x)
                     date_range = new_date.beginning_of_month..new_date.end_of_month
                     {
