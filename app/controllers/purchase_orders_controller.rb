1fb1b45aea0f29580b45b9202a674fe499e25293
flatironschool <danliu277@gmail.com>
2020-07-24 14:13:36 -0400

    Increase game stock after purchaseOrder created

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -14,12 +14,18 @@ class PurchaseOrdersController < ApplicationController
     def create_purchase_orders
         purchase_orders = []
         params['purchaseOrders'].each do |po|
-            purchase_orders.push(PurchaseOrder.create(
+            purchase_order = PurchaseOrder.create(
                 game_id: po['game_id'],
                 employee_id: po['employee_id'],
                 quantity: po['quantity'],
                 vendor_id: po['vendor_id']
-            ))
+            )
+            if purchase_order.id != nil
+                purchase_orders.push(purchase_order)
+                game = Game.find(purchase_order.game_id)
+                game.stock += purchase_order.quantity
+                game.save
+            end
         end
         render json: purchase_orders
     end
