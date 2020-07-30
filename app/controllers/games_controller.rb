ea3ffffda4a1363a84dc667e329bfc396f9e8da9
flatironschool <danliu277@gmail.com>
2020-07-30 14:34:50 -0400

    Sort games by id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,11 @@
 class GamesController < ApplicationController
     def index
-        games = Game.all
+        games = Game.all.sort_by{|game| game.id}
         render json: games
     end
 
     def show
-        game = Game.find(params[:id])
+        game = Game.find(params[:id]).sort_by{|game| game.id}
         render json: game
     end
 
